e538140aac9f7b472ce543b9b9516635006d3d73
mriswithe <1725647+mriswithe@users.noreply.github.com>
2020-12-29 16:13:50 -0600

    add get_block logic, copy/pastaed from the floor sheet for now. populate the CaveDecoSheet with two initial entries

--- src/modlunky2/sprites/deco_sheet.py
+++ src/modlunky2/sprites/deco_sheet.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
 from PIL import Image
 
@@ -12,7 +12,11 @@ number = Union[float, int]
 class AbstractDecoSheet(ABC):
     _sprite_sheet: Image.Image
     _base_path: Path
-    _chunk_map: chunk_map_type
+
+    @property
+    @abstractmethod
+    def chunk_map(self) -> chunk_map_type:
+        raise NotImplementedError
 
     def __init__(self, base_path: Path):
         self._base_path = base_path
@@ -25,8 +29,25 @@ class AbstractDecoSheet(ABC):
 
     @property
     def _sheet_path(self):
-        return Path(f"Data/Textures/floor_{self.biome_name}.png")
+        return Path(f"Data/Textures/deco_{self.biome_name}.png")
+
+    def _get_block(
+        self,
+        left: Union[int, float],
+        upper: Union[int, float],
+        right: Union[int, float],
+        lower: Union[int, float],
+    ) -> Image.Image:
+        """Used to get chunks of the sprite sheet."""
+        bbox = tuple(map(lambda x: x * 128, (upper, left, lower, right)))
+        return self._sprite_sheet.crop(bbox)
+
+    def get(self, name: str) -> Optional[Image.Image]:
+        coords = self.chunk_map.get(name)
+        if coords:
+            return self._get_block(*coords)
 
 
 class CaveDecoSheet(AbstractDecoSheet):
     biome_name = "cave"
+    chunk_map = {"kali_bg": (0, 0, 5, 4), "log_trap": (5, 1, 10, 3)}
