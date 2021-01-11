f2e75efe7240f4507e72da9d0828aa6cca12cb6b
mriswithe <1725647+mriswithe@users.noreply.github.com>
2021-01-10 18:03:09 -0600

    add in monster sheets and make the non_biome_sheets a little easier to add onto

--- src/modlunky2/sprites/sprite_fetcher.py
+++ src/modlunky2/sprites/sprite_fetcher.py
@@ -1,23 +1,16 @@
-from itertools import chain
 from pathlib import Path
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, List, Optional, Tuple
 
 from PIL import Image
 
-from .base_classes import AbstractBiome, DEFAULT_BASE_PATH
-from .floormisc import FloorMiscSheet
-from .hud import HudSheet
-from .items import ItemSheet
+from .base_classes import AbstractBiome, BaseSpriteLoader, DEFAULT_BASE_PATH
 
 
 class SpelukySpriteFetcher:
     def __init__(self, base_path: Path = DEFAULT_BASE_PATH):
         self.base_path = base_path
         # Setting up universal pieces
-        self._item_sheet = ItemSheet(self.base_path)
-        self._hud_sheet = HudSheet(self.base_path)
-        self._floor_misc = FloorMiscSheet(self.base_path)
-        self._non_biome_map = self._make_non_biome_map()
+        self._non_biome_sheets, self._non_biome_map = self._make_non_biome_map()
         # Now biome specific pieces
         self._biome_dict = self._init_biomes()
         self._biome_map = {k: v.get for k, v in self._biome_dict.items()}
@@ -30,26 +23,20 @@ class SpelukySpriteFetcher:
             for b in biomes.__all__
         }
 
-    def _make_non_biome_map(self) -> Dict[str, Callable]:
-        item_key_map = self._item_sheet.key_map()
-        hud_key_map = self._hud_sheet.key_map()
-        floor_misc_key_map = self._floor_misc.key_map()
-        # checking for key collision between item/hud/floor sheets here
-        item_keys = set(item_key_map)
-        hud_keys = set(hud_key_map)
-        floor_misc_keys = set(floor_misc_key_map)
-        if len(item_keys | hud_keys | floor_misc_keys) != len(
-            list(chain(item_keys, hud_keys, floor_misc_keys))
-        ):
-            # TODO: Determine the key collision location
-            raise KeyError(
-                "Conflicting keys detected between item, hud, floor_misc keys"
-            )
-        non_biome_map = {}
-        non_biome_map.update(item_key_map)
-        non_biome_map.update(hud_key_map)
-        non_biome_map.update(floor_misc_key_map)
-        return non_biome_map
+    def _make_non_biome_map(self) -> Tuple[List[BaseSpriteLoader], Dict[str, Callable]]:
+        from . import monsters
+        from .items import ItemSheet
+        from .hud import HudSheet
+        from .floormisc import FloorMiscSheet
+
+        sheets = [getattr(monsters, m) for m in monsters.__all__]
+        sheets.extend([ItemSheet, HudSheet, FloorMiscSheet])
+        sheets = [s(self.base_path) for s in sheets]
+        key_map = {}
+        for sheet in sheets:
+            for k in sheet.key_map():
+                key_map[k] = sheet.get
+        return sheets, key_map
 
     # noinspection PyNoneFunctionAssignment
     def get(self, name: str, biome: str = "cave") -> Optional[Image.Image]:
