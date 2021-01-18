549bb9cf557e14a7eacb8e9914ee8ff060a375ca
Hanno <h.baensch.92@gmail.com>
2021-01-18 20:33:13 +0100

    Add option to store grid in a separate file

--- src/modlunky2/sprites/base_classes/base_sprite_merger.py
+++ src/modlunky2/sprites/base_classes/base_sprite_merger.py
@@ -3,6 +3,7 @@ from functools import wraps
 from pathlib import Path
 from typing import Callable, Dict, Optional, Union, List, Type, Tuple
 from random import uniform
+from os import makedirs
 
 from PIL import Image, ImageDraw, ImageFont
 
@@ -10,7 +11,7 @@ from .types import image_crop_tuple_whole_number
 from .base_sprite_loader import BaseSpriteLoader
 
 _DEFAULT_BASE_PATH = Path(
-    r"C:\Program Files (x86)\Steam\steamapps\common\Spelunky 2\Mods\Extracted"
+    r"D:\Program Files (x86)\SteamLibrary\steamapps\common\Spelunky 2\Mods\Extracted"
 )
 
 
@@ -41,22 +42,27 @@ class BaseSpriteMerger(ABC):
         """
         pass
 
-    def __init__(self, base_path: Path = _DEFAULT_BASE_PATH):
+    def __init__(
+        self, base_path: Path = _DEFAULT_BASE_PATH, separate_grid_file: bool = True
+    ):
         self.base_path = base_path
         self._full_path = self.base_path / self._target_sprite_sheet_path
+        self._separate_grid_file = separate_grid_file
+        self._grid_colors = [(255, 0, 0, 255), (0, 0, 255, 255)]
+        self._grid_colors_idx = 0
 
         max_image_width = 0
         total_image_height = 0
         for origin_def in self._origin_map.values():
             origin_chunk_size = (0, 0)
-            for name, coords in origin_def["chunk_map"].items():
+            for _, coords in origin_def["chunk_map"].items():
                 origin_chunk_size = (
                     max(origin_chunk_size[0], coords[2]),
                     max(origin_chunk_size[1], coords[3]),
                 )
-            extended_chunk_size = origin_def["chunk_size"] + 2 * self._grid_hint_size
+            real_chunk_size = self._get_real_chunk_size(origin_def["chunk_size"])
             origin_def["image_size"] = tuple(
-                image_size * extended_chunk_size for image_size in origin_chunk_size
+                image_size * real_chunk_size for image_size in origin_chunk_size
             )
             max_image_width = max(max_image_width, origin_def["image_size"][0])
             total_image_height = total_image_height + origin_def["image_size"][1]
@@ -64,23 +70,36 @@ class BaseSpriteMerger(ABC):
         image_size = (int(max_image_width), int(total_image_height))
         self._sprite_sheet = Image.new(mode="RGBA", size=image_size, color=(0, 0, 0, 0))
 
-        image_draw = ImageDraw.Draw(self._sprite_sheet)
-        guide_text = "Stay within the guides! Do not overlap the guides! "
-        default_font = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", size=25)
-        text_width, text_height = default_font.getsize(guide_text)
-        text_height_coord = 0
-        text_height_coord_start = 0
-        while text_height_coord < total_image_height:
-            text_width_coord = text_height_coord_start
-            while text_width_coord < max_image_width:
-                image_draw.text(
-                    (text_width_coord, text_height_coord), guide_text, font=default_font
-                )
-                text_width_coord += text_width
-            text_height_coord += text_height
-            text_height_coord_start -= text_width / 2 + uniform(
-                -text_width / 2, text_width / 2
+        if self._separate_grid_file:
+            self._grid_image = Image.new(
+                mode="RGBA", size=image_size, color=(0, 0, 0, 0)
             )
+        else:
+            self._grid_image = self._sprite_sheet
+        self._grid_image_draw = ImageDraw.Draw(self._grid_image)
+
+        def draw_text_hint(text_hint: str):
+            default_font = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", size=25)
+            lines = text_hint.splitlines()
+            current_height = 0
+            for line in lines:
+                line_width, line_height = default_font.getsize(line)
+                current_height = current_height + line_height
+                self._grid_image_draw.text(
+                    (image_size[0] - line_width, image_size[1] - current_height),
+                    line,
+                    font=default_font,
+                )
+
+        if self._separate_grid_file:
+            draw_text_hint("Stay within the guides!")
+        else:
+            draw_text_hint("Stay within the guides!\nDo not overlap the guides!")
+
+    def _get_real_chunk_size(self, chunk_size: int) -> int:
+        if self._separate_grid_file:
+            return chunk_size
+        return chunk_size + 2 * self._grid_hint_size
 
     def _get_image_coords(
         self,
@@ -91,13 +110,18 @@ class BaseSpriteMerger(ABC):
         chunk_size: int,
         height_offset: int,
     ) -> Tuple[image_crop_tuple_whole_number, image_crop_tuple_whole_number]:
+        if self._separate_grid_file:
+            bbox = (left, upper, right, lower)
+            bbox = tuple(x * self._get_real_chunk_size(chunk_size) for x in bbox)
+            bbox = (bbox[0], bbox[1] + height_offset, bbox[2], bbox[3] + height_offset)
+            return bbox, bbox
 
-        extended_chunk_size = chunk_size + 2 * self._grid_hint_size
-        bg_bbox = [left, upper, right, lower]
-        bg_bbox = [coord * extended_chunk_size for coord in bg_bbox]
-        bg_bbox[1] = bg_bbox[1] + height_offset
-        bg_bbox[3] = bg_bbox[3] + height_offset
-        bg_bbox = tuple(bg_bbox)
+        real_chunk_size = self._get_real_chunk_size(chunk_size)
+        grid_bbox = [left, upper, right, lower]
+        grid_bbox = [coord * real_chunk_size for coord in grid_bbox]
+        grid_bbox[1] = grid_bbox[1] + height_offset
+        grid_bbox[3] = grid_bbox[3] + height_offset
+        grid_bbox = tuple(grid_bbox)
 
         chunk_dimensions = (right - left, lower - upper)
         grid_extensions_for_internal_offset = [
@@ -105,20 +129,21 @@ class BaseSpriteMerger(ABC):
             for chunk_dimension in chunk_dimensions
         ]
         chunk_bbox = (
-            bg_bbox[0] + grid_extensions_for_internal_offset[0],
-            bg_bbox[1] + grid_extensions_for_internal_offset[1],
-            bg_bbox[2] - grid_extensions_for_internal_offset[0],
-            bg_bbox[3] - grid_extensions_for_internal_offset[1],
+            grid_bbox[0] + grid_extensions_for_internal_offset[0],
+            grid_bbox[1] + grid_extensions_for_internal_offset[1],
+            grid_bbox[2] - grid_extensions_for_internal_offset[0],
+            grid_bbox[3] - grid_extensions_for_internal_offset[1],
         )
 
-        return (bg_bbox, chunk_bbox)
+        return (grid_bbox, chunk_bbox)
 
-    def _put_background(self, left: int, upper: int, right: int, lower: int):
-        bg_bbox = (left, upper, right, lower)
-        bg_size = (right - left, lower - upper)
-        bg_color = (255, 0, 0, 255)
-        bg_image = Image.new(mode="RGBA", size=bg_size, color=bg_color)
-        self._sprite_sheet.paste(bg_image, bg_bbox)
+    def _put_grid(self, left: int, upper: int, right: int, lower: int):
+        grid_bbox = (left, upper, right, lower)
+        grid_color = self._grid_colors[self._grid_colors_idx]
+        self._grid_colors_idx = 1 if self._grid_colors_idx == 0 else 0
+        self._grid_image_draw.rectangle(
+            grid_bbox, outline=grid_color, width=self._grid_hint_size
+        )
 
     def _put_chunk(self, left: int, upper: int, right: int, lower: int, image: Image):
         bbox = (left, upper, right, lower)
@@ -134,10 +159,18 @@ class BaseSpriteMerger(ABC):
                 for name, coords in origin_def["chunk_map"].items():
                     source_image = sprite_loader.get(name)
                     if source_image:
-                        bg_coords, chunk_coords = self._get_image_coords(
+                        grid_coords, chunk_coords = self._get_image_coords(
                             *coords, chunk_size, height_offset
                         )
-                        self._put_background(*bg_coords)
+                        self._put_grid(*grid_coords)
                         self._put_chunk(*chunk_coords, source_image)
                 height_offset = height_offset + origin_def["image_size"][1]
-        return self._sprite_sheet.copy()
+        return self._sprite_sheet
+
+    def save(self):
+        if not self._target_sprite_sheet_path.parent.exists():
+            makedirs(self._target_sprite_sheet_path.parent)
+        self._sprite_sheet.save(self._target_sprite_sheet_path)
+        if self._separate_grid_file:
+            grid_file_path = f"{self._target_sprite_sheet_path.with_suffix('')}_grid{self._target_sprite_sheet_path.suffix}"
+            self._grid_image.save(grid_file_path)
