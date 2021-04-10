76e67d46c6f9205b4b7486451cdb7c027c416962
Gary M. Josack <gary@byoteki.com>
2021-04-09 22:41:14 -0700

    add destination ui for installer

--- src/modlunky2/ui/install.py
+++ src/modlunky2/ui/install.py
@@ -1,7 +1,6 @@
 import logging
-from pathlib import Path
-
 import tkinter as tk
+from pathlib import Path
 from tkinter import ttk
 
 from modlunky2.ui.widgets import Tab
@@ -9,7 +8,7 @@ from modlunky2.ui.widgets import Tab
 logger = logging.getLogger("modlunky2")
 
 
-class FileChooser(ttk.Frame):
+class SourceChooser(ttk.Frame):
     def __init__(self, parent, modlunky_config):
         super().__init__(parent)
         self.modlunky_config = modlunky_config
@@ -44,6 +43,8 @@ class FileChooser(ttk.Frame):
 
         filename = tk.filedialog.askopenfilenames(parent=self, initialdir=initial_dir)
         if not filename:
+            self.file_chooser_var.set("")
+            self.master.master.render()
             return
 
         filename = filename[0]
@@ -52,6 +53,79 @@ class FileChooser(ttk.Frame):
 
         self.modlunky_config.config_file.last_install_browse = str(parent.as_posix())
         self.modlunky_config.config_file.save()
+        self.master.master.render()
+
+
+class DestinationChooser(ttk.Frame):
+    def __init__(self, parent, modlunky_config):
+        super().__init__(parent)
+        self.modlunky_config = modlunky_config
+
+        file_chooser_label = ttk.Label(self, text="Choose or Create a Pack")
+        file_chooser_label.grid(
+            row=0, column=0, padx=5, pady=(2, 0), columnspan=3, sticky="w"
+        )
+
+        self.file_chooser_var = tk.StringVar()
+
+        file_chooser_entry = ttk.Entry(
+            self,
+            textvariable=self.file_chooser_var,
+            state=tk.DISABLED,
+            width=80,
+        )
+        file_chooser_entry.columnconfigure(0, weight=1)
+        file_chooser_entry.columnconfigure(1, weight=1)
+        file_chooser_entry.columnconfigure(2, weight=1)
+        file_chooser_entry.grid(
+            row=1, column=0, padx=10, pady=10, columnspan=3, sticky="n"
+        )
+
+        file_chooser_browse = ttk.Button(self, text="Browse", command=self.browse)
+        file_chooser_browse.grid(row=2, column=0, pady=5, padx=5, sticky="nsew")
+
+    def browse(self):
+        initial_dir = self.modlunky_config.install_dir / "Mods/Packs"
+        if not initial_dir.exists():
+            logger.critical("Expected Packs dir not found.")
+            return
+
+        directory = tk.filedialog.askdirectory(parent=self, initialdir=initial_dir)
+        if not directory:
+            self.file_chooser_var.set("")
+            self.master.master.render()
+            return
+
+        initial_dir = initial_dir.resolve()
+        directory = Path(directory).resolve()
+
+        if initial_dir == directory:
+            logger.critical(
+                "You must create a directory inside of Packs to install your mod into."
+            )
+            self.file_chooser_var.set("")
+            self.master.master.render()
+            return
+
+        # Directory chosen Packs or above
+        if initial_dir not in directory.parents:
+            logger.critical("You chose a directory outside of Packs which is invalid.")
+            self.file_chooser_var.set("")
+            self.master.master.render()
+            return
+
+        # Directory chosen inside of a pack directory
+        relative_pack_dir = directory.relative_to(initial_dir)
+        if len(relative_pack_dir.parts) != 1:
+            logger.critical(
+                "You must choose a directory only one level deep inside of Packs."
+            )
+            self.file_chooser_var.set("")
+            self.master.master.render()
+            return
+
+        self.file_chooser_var.set(relative_pack_dir)
+        self.master.master.render()
 
 
 class InstallTab(Tab):
@@ -64,19 +138,37 @@ class InstallTab(Tab):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, minsize=60)
 
         source_frame = ttk.LabelFrame(self, text="Source")
         source_frame.rowconfigure(0, weight=1)
         source_frame.columnconfigure(0, weight=1)
         source_frame.grid(row=0, column=0, pady=5, padx=5, sticky="nswe")
 
-        file_chooser_frame = FileChooser(source_frame, modlunky_config)
-        file_chooser_frame.grid(row=0, column=0, pady=5, padx=5, sticky="new")
+        self.file_chooser_frame = SourceChooser(source_frame, modlunky_config)
+        self.file_chooser_frame.grid(row=0, column=0, pady=5, padx=5, sticky="new")
 
         dest_frame = ttk.LabelFrame(self, text="Destination")
         dest_frame.rowconfigure(0, weight=1)
         dest_frame.columnconfigure(0, weight=1)
         dest_frame.grid(row=1, column=0, pady=5, padx=5, sticky="nswe")
 
-        file_chooser_frame2 = FileChooser(dest_frame, modlunky_config)
-        file_chooser_frame2.grid(row=0, column=0, pady=5, padx=5, sticky="new")
+        self.file_chooser_frame2 = DestinationChooser(dest_frame, modlunky_config)
+        self.file_chooser_frame2.grid(row=0, column=0, pady=5, padx=5, sticky="new")
+
+        self.button_install = ttk.Button(self, text="install", command=self.install)
+        self.button_install.grid(row=2, column=0, pady=5, padx=5, sticky="nswe")
+
+    def on_load(self):
+        self.render()
+
+    def install(self):
+        pass
+
+    def render(self):
+        source = self.file_chooser_frame.file_chooser_var.get()
+        dest = self.file_chooser_frame2.file_chooser_var.get()
+        if source and dest:
+            self.button_install["state"] = tk.NORMAL
+        else:
+            self.button_install["state"] = tk.DISABLED
