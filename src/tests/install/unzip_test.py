c364406817d5bfcb47be74362d3825f5b24fdbd0
Gary M. Josack <gary@byoteki.com>
2021-04-24 12:50:50 -0700

    actually add tests

---
+++ src/tests/install/unzip_test.py
@@ -0,0 +1,53 @@
+import zipfile
+from io import BytesIO
+
+from modlunky2.ui.install import get_zip_members
+
+
+def make_zip(in_files):
+    zip_file = zipfile.ZipFile(BytesIO(), "w")
+    for in_file in in_files:
+        zip_file.writestr(zipfile.ZipInfo(filename=in_file), b"")
+
+    return zip_file
+
+
+def compare_zip(in_files, out_files):
+    zip_file = make_zip(in_files)
+    for idx, out_file in enumerate(get_zip_members(zip_file)):
+        assert out_file.filename == out_files[idx]
+
+
+def test_rename_to_main_lua():
+    in_files = ["foo.lua"]
+    out_files = ["main.lua"]
+    compare_zip(in_files, out_files)
+
+    in_files = ["foo/foo.lua"]
+    out_files = ["main.lua"]
+    compare_zip(in_files, out_files)
+
+    in_files = ["foo/foo.lua", "bar.lua"]
+    compare_zip(in_files, in_files)
+
+
+def test_data_and_soundbank_no_unwrap():
+    in_files = ["Data/foo"]
+    compare_zip(in_files, in_files)
+
+    in_files = ["soundbank/foo"]
+    compare_zip(in_files, in_files)
+
+
+def test_unwrap_common_dir():
+    in_files = ["foo/test.png", "foo/bar.png"]
+    out_files = ["test.png", "bar.png"]
+    compare_zip(in_files, out_files)
+
+    in_files = ["foo/test.png", "foo/bar.png", "test"]
+    compare_zip(in_files, in_files)
+
+
+def test_bat_frend_regression():
+    in_files = ["main.lua", "mod_info.json", "custom_images/batty.png"]
+    compare_zip(in_files, in_files)
