308a2b979de083a591b507c9602740669a990bb6
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2021-10-29 01:32:41 -0600

    Add some comments, and also only create the temp dir if there is a save file.

--- src/modlunky2/ui/install.py
+++ src/modlunky2/ui/install.py
@@ -374,11 +374,12 @@ def install_fyi_mod(
     pack_dir = packs_dir / f"fyi.{install_code}"
     tmp_dir = packs_dir / f"temp_fyi.{install_code}"
 
-    tmp_dir.mkdir(parents=True, exist_ok=True)
     save_path = pack_dir / "save.dat"
     tmp_save_path = tmp_dir / "save.dat"
     if pack_dir.exists():
         if save_path.exists():
+            # Store the save file into a temp directory while downloading the new version.
+            tmp_dir.mkdir(parents=True, exist_ok=True)
             shutil.copy(save_path, tmp_save_path)
         if overwrite:
             logger.debug("Removing previous installation at %s", pack_dir)
@@ -427,6 +428,7 @@ def install_fyi_mod(
     write_manifest(pack_metadata_dir, mod_details, mod_file, logo_name)
 
     if tmp_save_path.exists():
+        # If there was a save file, move it into the new pack.
         shutil.copy(tmp_save_path, save_path)
         shutil.rmtree(tmp_dir)
 
