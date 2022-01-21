8a08e8d74aefe4a5c38f62a225c211d230bc5242
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2022-01-20 21:52:32 -0700

    Add a separate sprite for sticky traps so that the full size that the trap will take up in the level can be seen.

--- src/modlunky2/sprites/tilecode_extras.py
+++ src/modlunky2/sprites/tilecode_extras.py
@@ -111,7 +111,16 @@ class ChainAndBlocksCeilingSheet(BaseSpriteLoader):
     }
 
 
+class StickyTrapSheet(BaseSpriteLoader):
+    _sprite_sheet_path = Path("static/images/sticky_trap.png")
+    _chunk_size = 128
+    _chunk_map = {
+        "sticky_trap": (0, 0, 1, 2),
+    }
+
+
 EXTRA_TILECODE_CLASSES = [
-    TreasureVaultChestSheet,
     ChainAndBlocksCeilingSheet,
+    StickyTrapSheet,
+    TreasureVaultChestSheet,
 ]
