3b3822ccb02ec016c1f4a06dd6687a2a3e1d0997
Mauve <mauve@google.com>
2022-01-27 00:22:22 -0500

    Add session test, remove duplicate support

--- src/modlunky2/web/api/framework/session.py
+++ src/modlunky2/web/api/framework/session.py
@@ -23,7 +23,8 @@ class SessionManager:
     def session_for(self, websocket: WebSocket) -> Generator[SessionId, None, None]:
         sid = SessionId(websocket.path_params[self.route_sid_param_name])
         addr = websocket.client
-        if sid in self.sid_to_addr and self.sid_to_addr[sid] != addr:
+
+        if sid in self.sid_to_addr:
             logger.warning(
                 "Session %s already in use by %r, attempted reuse by %r",
                 sid,
@@ -31,7 +32,7 @@ class SessionManager:
                 addr,
             )
             raise SessionException(
-                f"Session ID {sid} is already used by another client"
+                f"Session ID {sid} is currently being used by another connection"
             )
         self.sid_to_addr[sid] = addr
 
