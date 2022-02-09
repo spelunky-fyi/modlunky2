438eddff2eeab13dd439ab0d56422c986cefd8e2
Mauve <mauve@google.com>
2022-02-08 23:50:13 -0500

    Add PubSub to demo

--- src/modlunky2/web/demo.py
+++ src/modlunky2/web/demo.py
@@ -1,12 +1,23 @@
+from contextlib import asynccontextmanager
+from functools import partial
+import logging
+import time
+from typing import NoReturn
+from anyio import create_task_group, current_time, sleep
+from dataclasses import dataclass
 from serde import serde
-from starlette.requests import Request
-from starlette.responses import PlainTextResponse
-
+from starlette.applications import Starlette
+from starlette.routing import Mount, Route
 from modlunky2.web.api.framework.multiplexing import (
     SendConnection,
     WSMultiplexer,
     WSMultiplexerRoute,
 )
+from modlunky2.web.api.framework.pubsub import PubSubManager, PubSubTopic, ServiceLevel
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
+
+logger = logging.getLogger("modlunky2")
 
 
 async def hello_world(_request: Request):
@@ -14,6 +25,7 @@ async def hello_world(_request: Request):
 
 
 @serde
+@dataclass
 class EchoMessage:
     message: str
 
@@ -22,4 +34,40 @@ async def echo(connection: SendConnection, msg: EchoMessage):
     await connection.send(msg)
 
 
-multiplexer = WSMultiplexer([WSMultiplexerRoute(EchoMessage, echo)])
+@serde
+@dataclass
+class TimeTick:
+    now: float
+
+
+async def ticker(pub_manager: PubSubManager) -> NoReturn:
+    period = 5  # in seconds
+    while True:
+        delay = period - current_time() % period
+        civil_time = time.time()
+
+        pub_manager.publish(TimeTick(civil_time))
+        await sleep(delay)
+
+
+@asynccontextmanager
+async def app_lifespan(pub_manager: PubSubManager, _app: Starlette):
+    async with create_task_group() as tg:
+        tg.start_soon(ticker, pub_manager)
+        yield
+        tg.cancel_scope.cancel()
+
+
+def make_app() -> Starlette:
+    topics = [PubSubTopic(TimeTick, ServiceLevel.MAY_DROP)]
+    pub_manager = PubSubManager(topics)
+    multiplexer = WSMultiplexer(
+        (WSMultiplexerRoute(EchoMessage, echo),) + pub_manager.multiplexer_routes
+    )
+    routes = [
+        Route("/", hello_world),
+        Mount("/echo", routes=[multiplexer.starlette_route]),
+    ]
+    return Starlette(
+        debug=True, routes=routes, lifespan=partial(app_lifespan, pub_manager)
+    )
--- src/modlunky2/web/service.py
+++ src/modlunky2/web/service.py
@@ -2,12 +2,11 @@ import threading
 import typing
 from typing import Callable
 
-import anyio
+from anyio import run
+from anyio.to_thread import run_sync
 from hypercorn.asyncio import serve
 from hypercorn.config import Config as HypercornConfig
 from hypercorn.typing import ASGI3Framework
-from starlette.applications import Starlette
-from starlette.routing import Mount, Route
 
 import modlunky2.web.demo as demo
 
@@ -25,18 +24,13 @@ def launch_in_thread() -> Callable[[], None]:
 
 
 def _async_worker(shutting_down: threading.Event):
-    routes = [
-        Route("/", demo.hello_world),
-        Mount("/echo", routes=[demo.multiplexer.starlette_route]),
-    ]
-    app = Starlette(debug=True, routes=routes)
-
+    app = demo.make_app()
     hypercorn_conf = HypercornConfig()
     hypercorn_conf.bind = "127.0.0.1:9526"
     hypercorn_conf.websocket_ping_interval = 30.0
 
     async def shutdown_trigger():
-        await anyio.to_thread.run_sync(shutting_down.wait)
+        await run_sync(shutting_down.wait)
 
     async def _serve():
         await serve(
@@ -46,4 +40,4 @@ def _async_worker(shutting_down: threading.Event):
             shutdown_trigger=shutdown_trigger,
         )
 
-    anyio.run(_serve)
+    run(_serve)
