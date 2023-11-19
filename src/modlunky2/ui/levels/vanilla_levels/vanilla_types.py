eb982b8817f657403aeabfc53a890c515f3082b0
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2023-11-18 23:43:32 -0700

    Run formatter.

--- src/modlunky2/ui/levels/vanilla_levels/vanilla_types.py
+++ src/modlunky2/ui/levels/vanilla_levels/vanilla_types.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
 from typing import List, Optional
 
-from modlunky2.levels.level_templates import  TemplateSetting
+from modlunky2.levels.level_templates import TemplateSetting
 from modlunky2.ui.levels.shared.setrooms import MatchedSetroom
+
 
 @dataclass
 class RoomInstance:
