9c12ca02467d772bc3da594bb1cdbeaf075256d8
JayTheBusinessGoose <jaythebusinessgoose@gmail.com>
2023-12-22 19:46:24 -0700

    Add selection and fill controls to the canvas.

--- src/modlunky2/ui/levels/shared/level_canvas.py
+++ src/modlunky2/ui/levels/shared/level_canvas.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 import logging
 import math
 import tkinter as tk
@@ -17,18 +18,45 @@ class GridRoom:
     width: int
     height: int
 
+class CANVAS_MODE(Enum):
+    DRAW = 1
+    SELECT = 2
+    FILL = 3
+
+
+@dataclass
+class SelectRect:
+    x0: int
+    y0: int
+    x1: int
+    y1: int
+
+@dataclass
+class TileIndex:
+    x: int
+    y: int
+
+@dataclass
+class Selection:
+    pos: SelectRect
+    rect: int
+    first_tile: TileIndex
+    last_tile: TileIndex
 
 class LevelCanvas(tk.Canvas):
     def __init__(
-        self, parent, textures_dir, zoom_level, on_click, on_pull_tile, *args, **kwargs
+        self, parent, textures_dir, zoom_level, on_click, on_pull_tile, on_fill, on_fill_type, *args, **kwargs
     ):
         super().__init__(parent, *args, **kwargs)
-
         self.textures_dir = textures_dir
         self.zoom_level = zoom_level
 
         self.on_pull_tile = on_pull_tile
         self.on_click = on_click
+        self.on_fill = on_fill
+        self.on_fill_type = on_fill_type
+
+        self.mode = CANVAS_MODE.DRAW
 
         self.tile_images = []
         self.grid_hidden = False
@@ -42,43 +70,91 @@ class LevelCanvas(tk.Canvas):
         self.width = 0
         self.height = 0
 
-        if on_pull_tile:
-            shift_down = False
+        self.shift_down = False
 
-            def holding_shift(_):
-                nonlocal shift_down
-                if shift_down:
-                    return
-                shift_down = True
+        def holding_shift(_):
+            if self.shift_down:
+                return
+            self.shift_down = True
+
+            if self.mode == CANVAS_MODE.DRAW:
                 self.config(cursor="pencil")
 
-            def shift_up(_):
-                nonlocal shift_down
-                if not shift_down:
-                    return
-                shift_down = False
+        def shift_up(_):
+            if not self.shift_down:
+                return
+            self.shift_down = False
+            if self.mode == CANVAS_MODE.DRAW:
                 self.config(cursor="")
 
-            self.bind_all("<KeyPress-Shift_L>", holding_shift, add="+")
-            self.bind_all("<KeyPress-Shift_R>", holding_shift, add="+")
-            self.bind_all("<KeyRelease-Shift_L>", shift_up, add="+")
+        self.bind_all("<KeyPress-Shift_L>", holding_shift, add="+")
+        self.bind_all("<KeyPress-Shift_R>", holding_shift, add="+")
+        self.bind_all("<KeyRelease-Shift_L>", shift_up, add="+")
 
+        self.space_down = False
+        def holding_space(_):
+            self.space_down = True
+
+        def space_up(_):
+            self.space_down = False
+
+        self.bind_all("<KeyPress- >", holding_space, add="+")
+        self.bind_all("<KeyRelease- >", space_up, add="+")
+
+        self.select_rects = []
+        self.current_select_rect = None
+        if on_fill and on_fill_type:
+            # Click actions performed when selecting a region.
+            self.bind("<Button-1>", self.select_click, add="+")
+            self.bind("<B1-Motion>", self.select_drag, add="+")
+            self.bind("<ButtonRelease-1>", self.select_release, add="+")
+            self.bind("<Shift-Button-1>", self.select_click, add="+")
+            self.bind("<Shift-B1-Motion>", self.select_drag, add="+")
+            self.bind("<Shift-ButtonRelease-1>", self.select_release, add="+")
+
+
+            self.bind("<Button-1>", self.click_fill, add="+")
+            self.bind("<Button-3>", self.click_fill, add="+")
+
+        if on_pull_tile:
             # Click actions performed when holding shift to select the tile at the cursor's
             # location.
-            self.bind("<Shift-Button-1>", self.shift_click)
-            self.bind("<Shift-Button-3>", self.shift_click)
-            self.bind("<Shift-B1-Motion>", lambda event: None)
-            self.bind("<Shift-B3-Motion>", lambda event: None)
+            self.bind("<Shift-Button-1>", self.shift_click, add="+")
+            self.bind("<Shift-Button-3>", self.shift_click, add="+")
+            self.bind("<Shift-B1-Motion>", lambda event: None, add="+")
+            self.bind("<Shift-B3-Motion>", lambda event: None, add="+")
 
         if on_click:
             # Click actions performed when the shift key is not down to "draw" the currently
             # selected tile at the cursor's position.
-            self.bind("<Button-1>", self.click)
-            self.bind("<B1-Motion>", self.click)
-            self.bind("<Button-3>", self.click)
-            self.bind("<B3-Motion>", self.click)
+            self.bind("<Button-1>", self.click, add="+")
+            self.bind("<B1-Motion>", self.click, add="+")
+            self.bind("<Button-3>", self.click, add="+")
+            self.bind("<B3-Motion>", self.click, add="+")
+
+    def set_mode(self, mode):
+        self.mode = mode
+        if self.shift_down and (self.mode == CANVAS_MODE.DRAW or self.mode == CANVAS_MODE.FILL):
+            self.config(cursor="pencil")
+        elif mode == CANVAS_MODE.FILL:
+            self.config(cursor="spraycan")
+        else:
+            self.config(cursor="")
+
+
+    def pos_in_selection(self, row, column):
+        for selection in self.select_rects:
+            first_tile = selection.first_tile
+            last_tile = selection.last_tile
+            if column >= first_tile.x and column <= last_tile.x and row >= first_tile.y and row <= last_tile.y:
+                return True
+
+        return False
 
     def click(self, event):
+        if self.mode != CANVAS_MODE.DRAW:
+            return
+        self.clear_selections()
         is_primary = event.num == 1 or event.state & 0x0100 == 0x0100
         column = int(event.x // self.zoom_level)
         row = int(event.y // self.zoom_level)
@@ -90,7 +166,22 @@ class LevelCanvas(tk.Canvas):
         self.on_click(row, column, is_primary)
 
     def shift_click(self, event):
-        is_primary = event.num == 1
+        if self.mode == CANVAS_MODE.DRAW or self.mode == CANVAS_MODE.FILL:
+            is_primary = event.num == 1
+            column = int(event.x // self.zoom_level)
+            row = int(event.y // self.zoom_level)
+            if column < 0 or event.x > int(self["width"]):
+                return
+            if row < 0 or event.y > int(self["height"]):
+                return
+
+            self.on_pull_tile(row, column, is_primary)
+
+
+    def click_fill(self, event):
+        if self.mode != CANVAS_MODE.FILL:
+            return
+        is_primary = event.num == 1 or event.state & 0x0100 == 0x0100
         column = int(event.x // self.zoom_level)
         row = int(event.y // self.zoom_level)
         if column < 0 or event.x > int(self["width"]):
@@ -98,7 +189,131 @@ class LevelCanvas(tk.Canvas):
         if row < 0 or event.y > int(self["height"]):
             return
 
-        self.on_pull_tile(row, column, is_primary)
+        tiles = []
+
+
+        if len(self.select_rects) > 0:
+            if not self.pos_in_selection(row, column):
+                return
+            for c in range(self.width):
+                for r in range(self.height):
+                    if self.pos_in_selection(r, c):
+                        tiles.append(TileIndex(x=c, y=r))
+            self.on_fill(tiles, is_primary)
+        elif self.on_fill_type:
+            self.on_fill_type(row, column, is_primary)
+
+    def update_selection_tiles(self, selection):
+        pos = selection.pos
+        minposx = min(pos.x0, pos.x1)
+        maxposx = max(pos.x0, pos.x1)
+        minposy = min(pos.y0, pos.y1)
+        maxposy = max(pos.y0, pos.y1)
+        minx = minposx // self.zoom_level
+        maxx = maxposx // self.zoom_level
+        miny = minposy // self.zoom_level
+        maxy = maxposy // self.zoom_level
+
+
+        if (minx < 0 and maxx < 0) or (minx >= self.width and maxx >= self.width) or (miny < 0 and maxy < 0) or (miny >= self.height and maxy >= self.height):
+            min_tile = None
+            max_tile = None
+        elif (maxposx - minposx < 2) and (maxposy - minposy < 2):
+            min_tile = None
+            max_tile = None
+        else:
+            if minx < 0:
+                minx = 0
+            if maxx < 0:
+                maxx = 0
+            if miny < 0:
+                miny = 0
+            if maxy < 0:
+                maxy = 0
+            if minx >= self.width:
+                minx = self.width - 1
+            if maxx >= self.width:
+                maxx = self.width - 1
+            if miny >= self.height:
+                miny = self.height - 1
+            if maxy >= self.height:
+                maxy = self.height - 1
+
+            min_tile = TileIndex(minx, miny)
+            max_tile = TileIndex(maxx, maxy)
+
+        selection.first_tile = min_tile
+        selection.last_tile = max_tile
+
+    def clear_selections(self):
+        for rect in self.select_rects:
+            self.delete(rect.rect)
+            self.select_rects = []
+
+    def bring_selections_to_front(self):
+        for rect in self.select_rects:
+            self.tag_raise(rect.rect)
+
+    def select_click(self, event):
+        if self.mode != CANVAS_MODE.SELECT:
+            return
+        select_coords = SelectRect(event.x, event.y, event.x, event.y)
+        if not self.shift_down:
+            self.clear_selections()
+
+        rect = self.create_rectangle(
+            event.x,
+            event.y,
+            event.x,
+            event.y,
+            outline="#F0F0F0",
+            fill="#F0F0F0",
+            width=2,
+            stipple="gray12",
+            state="normal",
+        )
+
+        self.current_select_rect = Selection(select_coords, rect, None, None)
+        self.select_rects.append(self.current_select_rect)
+        self.update_selection_tiles(self.current_select_rect)
+
+
+    def select_drag(self, event):
+        if self.mode != CANVAS_MODE.SELECT:
+            return
+        if self.current_select_rect == None:
+            return
+
+
+        pos = self.current_select_rect.pos
+        if self.space_down:
+            diffx, diffy = event.x - pos.x1, event.y - pos.y1
+            pos.x0 += diffx
+            pos.y0 += diffy
+        # else:
+        pos.x1 = event.x
+        pos.y1 = event.y
+
+        self.coords(self.current_select_rect.rect, pos.x0, pos.y0, pos.x1, pos.y1)
+        self.update_selection_tiles(self.current_select_rect)
+
+    def select_release(self, event):
+        if self.mode != CANVAS_MODE.SELECT:
+            return
+
+        pos = self.current_select_rect.pos
+        first_tile = self.current_select_rect.first_tile
+        last_tile = self.current_select_rect.last_tile
+
+        if first_tile is None or last_tile is None:
+            self.delete(self.current_select_rect.rect)
+            self.select_rects.remove(self.current_select_rect)
+        else:
+            pos.x0, pos.y0 = first_tile.x * self.zoom_level, first_tile.y * self.zoom_level
+            pos.x1, pos.y1 = (last_tile.x + 1) * self.zoom_level, (last_tile.y + 1) * self.zoom_level
+            self.coords(self.current_select_rect.rect, pos.x0, pos.y0, pos.x1, pos.y1)
+
+        self.current_select_rect = None
 
     def set_zoom(self, zoom_level):
         self.zoom_level = zoom_level
@@ -118,6 +333,8 @@ class LevelCanvas(tk.Canvas):
             image=image,
             anchor="nw",
         )
+
+        self.bring_selections_to_front()
 
     def configure_size(self, width, height):
         self.width = width
